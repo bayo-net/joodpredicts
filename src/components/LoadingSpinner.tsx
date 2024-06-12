a8baa5c0b507bd5c47e94269c49401290e69f813
bayo-net <bayonet_dev@protonmail.com>
2024-06-12 22:03:22 +0530

    fix: add message

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -9,6 +9,10 @@ export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({}) => {
                 <div className="flex justify-center items-center h-screen">
                     <div className="rounded-full h-20 w-20 bg-violet-800 animate-ping"></div>
                 </div>
+                <p>
+                    Please be patient and donot close this page, it can take up
+                    to 30 seconds
+                </p>
             </div>
         </div>
     )
